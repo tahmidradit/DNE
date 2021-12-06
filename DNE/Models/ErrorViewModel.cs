d82d888973d8144dc5d14768fdcff7a4d55895d2
tahmidradit <tahmid.radit@outlook.com>
2021-12-06 23:32:04 +0600

    Added project files

---
+++ DNE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DNE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
